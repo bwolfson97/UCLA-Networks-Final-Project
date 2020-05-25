97a6e3ff876dac7c1112e241401304e1326a6c08
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2020-05-24 21:18:25 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,13 +5,14 @@ import matplotlib as plt
 # globals, per day cases
 birth_rate = 50
 release_rate = 50
-death_rate = 50
+death_rate = # 0.2 % of recovered
 
 def recalibrate_graph(data):
   newdata = add_nodes(remove_nodes(data))
   return newdata
   
 def remove_nodes(data):
+  # take all recovered, and delete $death_rate$ fraction of them
   return data
 
 def add_nodes(data):
