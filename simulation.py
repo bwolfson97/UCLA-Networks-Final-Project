391c9165058fdca66fab8c67e6d21901d9494416
Brandon Wolfson <bwolfson97@gmail.com>
2020-05-25 15:16:32 -0700

    Fixed bug causing time 0 to be removed

--- simulation.py
+++ simulation.py
@@ -64,23 +64,25 @@ def process_data(data_list, death_rate: float):
         R: # of recovered inmates at each time step
         D: # of dead inmates at each time step
     """
-    times_l = []
-    susceptible_ll = []
-    infected_ll = []
-    recovered_ll = []
+    # Get t, S, I, R data from first time step
+    first_time, first_dict_of_states = data_list[0].summary()
+    times_ll = [first_time]
+    susceptible_ll = [first_dict_of_states['S']]
+    infected_ll = [first_dict_of_states['I']]
+    recovered_ll = [first_dict_of_states['R']]
 
-    # Get t, S, I, R data from data_list and correct them
-    for data in data_list:
+    # For next time steps, get data, but delete first element of each time step to fix "recovered bug"
+    for data in data_list[1:]:
         times, dict_of_states = data.summary()
 
         # Append each time's data to appropriate list
-        times_l.append(np.delete(times, 0))
+        times_ll.append(np.delete(times, 0))
         susceptible_ll.append(np.delete(dict_of_states['S'], 0))  # Deletes first element because of "recovered bug"
         infected_ll.append(np.delete(dict_of_states['I'], 0))
         recovered_ll.append(np.delete(dict_of_states['R'], 0))
 
     # Aggregate quantities into single lists
-    t, S, I, R = aggregate_quantity(times_l), aggregate_quantity(susceptible_ll), \
+    t, S, I, R = aggregate_quantity(times_ll), aggregate_quantity(susceptible_ll), \
                  aggregate_quantity(infected_ll), aggregate_quantity(recovered_ll)
 
     # Calculate deaths
