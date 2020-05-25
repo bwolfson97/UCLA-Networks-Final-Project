9848cae3a67067543d1bb178d048370c579ac71d
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2020-05-24 21:29:16 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,22 +1,24 @@
 import EoN
 import numpy as np
 import matplotlib as plt
+import networkx as nx
 
 # globals, per day cases
 birth_rate = 50
 release_rate = 50
 death_rate = 0.2 # % of recovered
 
-def recalibrate_graph(data):
-  newdata = add_nodes(remove_nodes(data))
-  return newdata
+def recalibrate_graph(G):
+  G_new = add_nodes(remove_nodes(G))
+  return G_new
   
-def remove_nodes(data):
+def remove_nodes(G):
   # take all recovered and delete $death_rate$ fraction of them
   # take all nodes and delete $release_rate$ many of them
-  return data
+  return G
 
-def add_nodes(data):
-  # add $birth_rate$ many susceptible nodes
-  return data
+def add_nodes(G):
+  for i in range(birth_rate):
+    G.add_node()
+  return G
 
