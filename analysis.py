7150cfda5517bb638dd9c0f4efad7710a7dde2ca
Brandon Wolfson <bwolfson97@gmail.com>
2020-05-28 18:31:37 -0700

    Cleaned up run-time print statements

--- analysis.py
+++ analysis.py
@@ -7,7 +7,7 @@ import numpy as np
 def summary(t, S, I, R, D, save_plot, title, parameters):
     """Plots graph of simulation and computes various statistics."""
     # Print parameters
-    print('############################## Parameters ##############################')
+    print('############################ Parameters #############################')
     print(parameters)
 
     # Print statistics
